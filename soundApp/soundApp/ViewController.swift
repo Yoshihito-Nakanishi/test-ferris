aee75f05cf4242ea1783394bf499368e369caf36
Yoshihito Nakanishi <yoshihito.nakanishi@gmail.com>
2023-07-10 18:14:40 +0900

    updated sound app

--- soundApp/soundApp/ViewController.swift
+++ soundApp/soundApp/ViewController.swift
@@ -9,15 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var testButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         LibpdManager.shared.setup()
         LibpdManager.shared.enable(true)
         LibpdManager.shared.openPatch("test.pd")
+        
 
     }
 
-
+    @IBAction func testButtonTapped(_ sender: Any) {
+        PdBase.sendBang(toReceiver: "test")
+    }
+    
 }
 
